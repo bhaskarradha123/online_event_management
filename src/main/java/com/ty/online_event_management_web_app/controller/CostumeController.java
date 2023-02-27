5d865cf597d4596595a7c162a1965ce97df7c9fe
venusurya <venusurya412@gmail.com>
2023-02-27 14:18:55 +0530

    service and controller layers modified

---
+++ src/main/java/com/ty/online_event_management_web_app/controller/CostumeController.java
@@ -0,0 +1,64 @@
+package com.ty.online_event_management_web_app.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.online_event_management_web_app.dto.Costume;
+import com.ty.online_event_management_web_app.service.CostumeService;
+import com.ty.online_event_management_web_app.util.ResponseStructure;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+public class CostumeController {
+
+	@Autowired
+	private CostumeService service;
+
+	@PostMapping("/costume")
+	@ApiOperation(value = "save Costume", notes = "Api is used to save List of Costume ")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Sucessfully Created ") })
+	public ResponseEntity<ResponseStructure<List<Costume>>> saveUser(@Valid @RequestBody List<Costume> costume,
+			@RequestParam String email) {
+		return service.saveCostume(costume, email);
+	}
+
+	@PutMapping("/costume")
+	@ApiOperation(value = "update Costume", notes = "Api is used to update Costume with given Costume Id")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Sucessfully updated "),
+			@ApiResponse(code = 404, message = "Id is  not Found for the given Costume Id") })
+	public ResponseEntity<ResponseStructure<Costume>> updateCostume(@RequestParam int id,
+			@Valid @RequestBody Costume costume) {
+		return service.updateCostume(costume, id);
+	}
+
+	@DeleteMapping("/costume")
+	@ApiOperation(value = "delete Costume", notes = "Api is used to delete  Costume with given Costume Id")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Sucessfully deleted "),
+			@ApiResponse(code = 404, message = "Id is  not Found for the given Costume Id") })
+	public ResponseEntity<ResponseStructure<Costume>> deleteCostume(@RequestParam int id) {
+		return service.deleteCostume(id);
+	}
+
+	@GetMapping("/costume")
+	@ApiOperation(value = "display costume", notes = "Api is used to display Costume with given Costume ID")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Sucessfully found to display "),
+			@ApiResponse(code = 404, message = " Id is not Found for the given Costume Id") })
+	public ResponseEntity<ResponseStructure<Costume>> getCostumeById(@RequestParam int id) {
+		return service.getCostumeById(id);
+	}
+
+}
---
+++ src/main/java/com/ty/online_event_management_web_app/service/UserOcassionService.java
@@ -0,0 +1,104 @@
+package com.ty.online_event_management_web_app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ty.online_event_management_web_app.dao.CustomerDao;
+import com.ty.online_event_management_web_app.dao.UserOccasionDao;
+import com.ty.online_event_management_web_app.dto.Costume;
+import com.ty.online_event_management_web_app.dto.MakeUp;
+import com.ty.online_event_management_web_app.dto.Organizer;
+import com.ty.online_event_management_web_app.dto.User;
+import com.ty.online_event_management_web_app.dto.UserOccassion;
+import com.ty.online_event_management_web_app.exception.NoSuchElementFoundByUserOccassionServiceException;
+import com.ty.online_event_management_web_app.repo.CostumeRepo;
+import com.ty.online_event_management_web_app.util.ResponseStructure;
+
+@Service
+public class UserOcassionService {
+
+	@Autowired
+	private UserOccasionDao dao;
+
+	@Autowired
+	private CustomerDao customerDao;
+
+	public ResponseEntity<ResponseStructure<UserOccassion>> saveUserOccassion(UserOccassion occassion, String email) {
+
+		User user = customerDao.getUserByEmail(email);
+		if (user != null) {
+			ResponseStructure<UserOccassion> responseStructure = new ResponseStructure<>();
+			responseStructure.setMessage("Costume Is Saved Sucessfully");
+			responseStructure.setStatus(HttpStatus.CREATED.value());
+			responseStructure.setData(dao.saveOccassion(occassion, email));
+			return new ResponseEntity<ResponseStructure<UserOccassion>>(responseStructure, HttpStatus.CREATED);
+		} else {
+			throw new NoSuchElementFoundByUserOccassionServiceException();
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<UserOccassion>> updateUserOcassion(int id, UserOccassion occassion) {
+		UserOccassion occassiondb = dao.getOccassionById(id);
+		ResponseStructure<UserOccassion> responseStructure = new ResponseStructure<>();
+		if (occassiondb != null) {
+
+			responseStructure.setMessage("Sucessfully User Ocassion is Update");
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setData(dao.updateOccasion(occassion, id));
+			return new ResponseEntity<ResponseStructure<UserOccassion>>(responseStructure, HttpStatus.OK);
+		} else {
+
+			throw new NoSuchElementFoundByUserOccassionServiceException();
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<UserOccassion>> deleteUserOccassion(int id) {
+		UserOccassion occassiondb = dao.getOccassionById(id);
+		ResponseStructure<UserOccassion> responseStructure = new ResponseStructure<>();
+		if (occassiondb != null) {
+
+			responseStructure.setMessage("Sucessfully User Ocassion is Deletd");
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setData(dao.deleteOccassion(id));
+			return new ResponseEntity<ResponseStructure<UserOccassion>>(responseStructure, HttpStatus.OK);
+		} else {
+
+			throw new NoSuchElementFoundByUserOccassionServiceException();
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<UserOccassion>> getUserOccassionById(int id) {
+		UserOccassion occassiondb = dao.getOccassionById(id);
+		ResponseStructure<UserOccassion> responseStructure = new ResponseStructure<>();
+		if (occassiondb != null) {
+
+			responseStructure.setMessage("Sucessfully User Ocassion is Found");
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setData(dao.getOccassionById(id));
+			return new ResponseEntity<ResponseStructure<UserOccassion>>(responseStructure, HttpStatus.OK);
+		} else {
+
+			throw new NoSuchElementFoundByUserOccassionServiceException();
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<List<UserOccassion>>> getListOfUsers(String email) {
+		User user = customerDao.getUserByEmail(email);
+		ResponseStructure<List<UserOccassion>> responseStructure = new ResponseStructure<>();
+		if (user != null) {
+
+			responseStructure.setMessage("Sucessfully User Ocassion is Found");
+			responseStructure.setStatus(HttpStatus.OK.value());
+			responseStructure.setData(dao.getListOccasionByUserEmail(email));
+			return new ResponseEntity<ResponseStructure<List<UserOccassion>>>(responseStructure, HttpStatus.OK);
+		} else {
+
+			throw new NoSuchElementFoundByUserOccassionServiceException();
+		}
+	}
+
+}
